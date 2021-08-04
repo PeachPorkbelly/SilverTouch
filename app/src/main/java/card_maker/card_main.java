7758029d86da2c5b3ff6980de33e4fe30602dfe8
artificial-lua <artificialluamail@gmail.com>
2021-08-05 04:21:01 +0900

    카드 메인

--- app/src/main/java/card_maker/card_main.java
+++ app/src/main/java/card_maker/card_main.java
@@ -1,15 +1,27 @@
 package card_maker;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.silvertouch.R;
 
 public class card_main extends AppCompatActivity {
 
+    ImageView card_image;
+    Button btn_left, btn_right, btn_close, btn_emoji, btn_clear, btn_ok;
+
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.card_main);
 
-
+        card_image = (ImageView) findViewById(R.id.card_image);
+        btn_left = (Button) findViewById(R.id.btn_left);
+        btn_right = (Button) findViewById(R.id.btn_right);
+        btn_close = (Button) findViewById(R.id.btn_close);
+        btn_emoji = (Button) findViewById(R.id.btn_emoji);
+        btn_clear = (Button) findViewById(R.id.btn_clear);
+        btn_ok = (Button) findViewById(R.id.btn_ok);
     }
 }
