fe7b770ba81dcee44f1c27fbf1c13d77dff26600
artificial-lua <artificialluamail@gmail.com>
2021-09-18 02:20:57 +0900

    vector_test.java [클래스 변경점] -- 주석 가시성 수정 [선언부 변경점] -- 이미지뷰의 선언을 for문으로 변경 리소스 내 Drawable 파일들은 int로 path가 저장되므로, int형 Array로 선언하여 일괄저장한 후, 미리 선언되었던 ArrayList에 for문으로 연결할 수 있습니다. 이후 다루는 숫자에 변동이 생긴 경우, OnCreate나 함수에서의 코드수정 없이 path가 저장된 Array만을 수정하여 적용할 수 있습니다. -- btn_wateringCan의 리스터에 분기 추가 wateringCan이 1개 이상일 경우에만 작동하도록 수정하였습니다. 0 이하일 경우 물뿌리개가 부족하다 메시지를 띄우도록 하였습니다. -- btn_init 초기화 버튼 구현 -- btn_wateringCanUp 물뿌리개 추가 버튼 구현 위 두 버튼은 테스트만을 위한 버튼으로 추론하여 큰 동작을 설계하지 않았습니다. 테스트용 기능 구현만 하였습니다. [함수 변경점] -- garden_refresh() 함수 생성 및 구현 반복되는 코드인 si.getInt 부분과 wateringCanNum.setText 부분을 함수로 구현하고, setFlower까지 작동시키도록 하였습니다. 수치가 변한 후 사용되도록 하였습니다. -- save_setter() 함수 생성 및 구현 현재 수치를 저장하는 코드 반복을 함수로 구현하고 사용되도록 하였습니다. -- setFlow()내 사용되는 변수를 수정하였습니다. defaultPlaceStartCount, defaultPlaceEndCount 변수는 해당 함수 내에서만 사용되고, 사용 전 반드시 초기화가 되어지는것으로 보입니다. 그리하여 전역변수에서 함수 내 지역변수로 수정하였습니다. setFlow() 함수는 차후 수정이 필요합니다. 개화 이미지를 전달받으면 수정할 수 있습니다. -- 편의성을 위한 logs()함수를 생성하였습니다. Toast및 System.out을 일괄적으로 처리하고, 코드 작성의 간편화를 위해 함수를 작성하였습니다.

--- app/src/main/java/mission/hangjeong/vector_test.java
+++ app/src/main/java/mission/hangjeong/vector_test.java
@@ -6,6 +6,7 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,35 +18,57 @@ import com.example.silvertouch.SavedInfo;
 import org.w3c.dom.Text;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+
+
+
+/*********************
+1. SavedInfo()를 통해 현재 물뿌리개와 성장도를 받아옵니다.
+2. 성장도에 따라 보이는 이미지가 다름
+3. 물뿌리개를 누르면 물뿌리개 개수 -1개 해서 새로 저장
+
+1. 물뿌리개 누른 개수가 10 이하일때 -> 이미지 디폴트 위치 0~9까지 새싹으로 바꿈
+2. 물뿌리개 누른 개수가 11 이상일 때 -> 물뿌리개 개수 -11 위치는 꽃으로 바꿈
+3. 물뿌리개를 눌렀을 때
+	- 물뿌리개 누른 개수가 9 이하일 때 -> 물뿌리개 누른 개수 위치 새싹으로 바꿈+wateredCount+1 파일저장
+	- 물뿌리개 누른 개수가 10이상일 때 -> 물뿌리개 누른 개수 위치 꽃으로 바꾸고+wateredCount+1 파일저장
+*********************/
 
 public class vector_test extends AppCompatActivity {
 
-
-    //새싹,꽃 기본위치
-    ImageView defaultPlace_01,defaultPlace_02,defaultPlace_03,defaultPlace_04,defaultPlace_05,defaultPlace_06,defaultPlace_07,defaultPlace_08,defaultPlace_09,defaultPlace_10;
-
-    //꽃 자라날 위치를 담은 리스트
+    /*** 여기서부터 선언부입니다. ***/
+    // 꽃의 이미지 뷰 이미지 패스를 탐색하기 위한 사전 선언문
+    // 꽃 자라날 위치를 담은 리스트
     ArrayList<ImageView> defaultPlace = new ArrayList<>();
+    //꽃 위치 ImageView의 id값을 int 배열로 저장하여 차후 사용. 이후 확장성이 좋습니다.
+    int getDefaultPlace[] = {
+            R.id.mygarden_defaultPlace_01,
+            R.id.mygarden_defaultPlace_02,
+            R.id.mygarden_defaultPlace_03,
+            R.id.mygarden_defaultPlace_04,
+            R.id.mygarden_defaultPlace_05,
+            R.id.mygarden_defaultPlace_06,
+            R.id.mygarden_defaultPlace_07,
+            R.id.mygarden_defaultPlace_08,
+            R.id.mygarden_defaultPlace_09,
+            R.id.mygarden_defaultPlace_10
+            // 뷰가 차후 늘어나거나 줄어들 경우, 여기서 배열 숫자만 조절하면 자연스럽게 수정되어 연결됩니다.
+    };
 
+    //물뿌리개 숫자에 관한 변수명
+    //wateringCan 개수, wateringCan이 눌린 총 횟수
+    int wateringCan, wateredCount;
+    //초기화, 물뿌리개 수 up에 관한 버튼
+    Button btn_init, btn_wateringCanUp;
     //물뿌리개 버튼
     ImageButton btn_wateringCan;
-
-    //초기화, 물뿌리개 수 up
-    Button btn_init, btn_wateringCanUp;
-
-    //wateringCan 개수 보여줄 text
+    //wateringCan 개수 보여줄 TextView
     TextView wateringCanNum;
 
-    //wateringCan 개수
-    int wateringCan;
-
-    //wateringCan이 눌린 총 횟수
-    int wateredCount;
-
-    // defaultPlace Index때 쓸 변수
-    int defaultPlaceStartCount, defaultPlaceEndCount;
-
+    //SavedInfo를 si에 할당합니다.
     SavedInfo si = new SavedInfo();
+
+    /*** 최초 실행시 실행되는 실행문. ***/
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -53,97 +76,88 @@ public class vector_test extends AppCompatActivity {
 
         setContentView(R.layout.mygarden);
 
-        //꽃 기본 위치
-        defaultPlace_01 = (ImageView)findViewById(R.id.mygarden_defaultPlace_01);
-        defaultPlace_02 = (ImageView)findViewById(R.id.mygarden_defaultPlace_02);
-        defaultPlace_03 = (ImageView)findViewById(R.id.mygarden_defaultPlace_03);
-        defaultPlace_04 = (ImageView)findViewById(R.id.mygarden_defaultPlace_04);
-        defaultPlace_05 = (ImageView)findViewById(R.id.mygarden_defaultPlace_05);
-        defaultPlace_06 = (ImageView)findViewById(R.id.mygarden_defaultPlace_06);
-        defaultPlace_07 = (ImageView)findViewById(R.id.mygarden_defaultPlace_07);
-        defaultPlace_08 = (ImageView)findViewById(R.id.mygarden_defaultPlace_08);
-        defaultPlace_09 = (ImageView)findViewById(R.id.mygarden_defaultPlace_09);
-        defaultPlace_10 = (ImageView)findViewById(R.id.mygarden_defaultPlace_10);
-
+        //변수에 View를 연결시키는 코드
         //물뿌리개 버튼
         btn_wateringCan = (ImageButton)findViewById(R.id.mygarden_wateringCan);
-
-        //초기화. 물뿌리개 수 up
+        //초기화. 물뿌리개 수 up 버튼
         btn_init = (Button)findViewById(R.id.mygarden_init);
         btn_wateringCanUp = (Button)findViewById(R.id.mygarden_countUp);
-
-        //물뿌리개 수 보여주는 text
+        //물뿌리개 수 보여주는 TextView
         wateringCanNum = (TextView)findViewById(R.id.mygarden_wateringCanNum);
+        //Array에 int형으로 저장된 ViewId를 ImageView로 연결하여 저장합니다.
+        for(int i = 0; i < getDefaultPlace.length; i++){
+            defaultPlace.add((ImageView) findViewById(getDefaultPlace[i]));
+        }
 
-        /*
+        // 화면 초기화
+        garden_refresh();
 
-1. 인싸테스트가 끝나면 물뿌리개 저장
-2. 물뿌리개 개수를 받아서 띄움
-3. 물뿌리개를 누르면 물뿌리개 개수 -1개 해서 새로 저장
-4. 성장도를 받음
-5. 성장도에 따라 보이는 이미지가 다름
-
-1. 물뿌리개 누른 개수가 10 이하일때 -> 이미지 디폴트 위치 0~9까지 새싹으로 바꿈
-2. 물뿌리개 누른 개수가 11 이상일 때 -> 물뿌리개 개수 -11 위치는 꽃으로 바꿈
-3. 물뿌리개를 눌렀을 때
-	- 물뿌리개 누른 개수가 9 이하일 때 -> 물뿌리개 누른 개수 위치 새싹으로 바꿈+wateredCount+1 파일저장
-	- 물뿌리개 누른 개수가 10이상일 때 -> 물뿌리개 누른 개수 위치 꽃으로 바꾸고+wateredCount+1 파일저장
-
-         */
-
-        /*
-        1. wateringCan의 수를 불러온다
-         */
-        wateringCan = si.getInt(getApplicationContext(),"WateringCan");
-
-        /*
-        3.리스트에 위치 정해주기 -> 리스트에 추가
-         */
-        addDefaultLocation();
-
-
-
-        /*
-        2. wateringCan의 개수를 TextView에 띄운다 (옆에 물뿌리개 총 몇번 눌렸는지도)
-         */
-        wateringCanNum.setText("물뿌리개 수"+wateringCan+"성장도"+wateredCount);
-
-
-
-        // 꽃 배치 설정
-        setFlower();
-
+        // 버튼에 대한 구현
         // 물뿌리개 버튼 클릭 시 물뿌리개 보유 개수 -1, 물뿌리개 눌린 횟수 +1. 꽃 다시 보여주기
         btn_wateringCan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                wateringCan -= 1;
-                wateredCount += 1;
-                si.setInt(getApplicationContext(), "WateringCan",wateringCan);
-                si.setInt(getApplicationContext(), "Watered",wateredCount);
-                setFlower();
+                if (wateringCan >= 1){
+                    wateringCan -= 1;
+                    wateredCount += 1;
+                    save_setter();
+                } else{
+                    logs("물뿌리개 부족");
+                }
+                garden_refresh();
             }
+        });
+        // 초기화 버튼
+        btn_init.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                wateringCan = 0;
+                wateredCount = 0;
+                save_setter();
+                garden_refresh();
+            }
+        });
+        // 물뿌리개 추가 버튼
+        btn_wateringCanUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                wateringCan++;
+                save_setter();
+                garden_refresh();
+            }
+
         });
 
 
 
     }//onCreate End---
 
-    public void addDefaultLocation(){
-        defaultPlace.add(defaultPlace_01);
-        defaultPlace.add(defaultPlace_02);
-        defaultPlace.add(defaultPlace_03);
-        defaultPlace.add(defaultPlace_04);
-        defaultPlace.add(defaultPlace_05);
-        defaultPlace.add(defaultPlace_06);
-        defaultPlace.add(defaultPlace_07);
-        defaultPlace.add(defaultPlace_08);
-        defaultPlace.add(defaultPlace_09);
-        defaultPlace.add(defaultPlace_10);
+    /*** 각 함수에 관한 구현입니다. ***/
+
+    // refresh가 필요한 경우 실행합니다.
+    public void garden_refresh(){ // garden 내에서 refresh가 필요한 모든 동작을 여기서 처리합니다.
+        wateringCan = si.getInt(getApplicationContext(),"WateringCan");
+        wateredCount = si.getInt(getApplicationContext(),"Watered");
+        // 물뿌리개 숫자 및 성장도 리프레시
+        wateringCanNum.setText("물뿌리개 수"+wateringCan+"성장도"+wateredCount);
+
+        setFlower();
     }
 
+    // 수치가 변하는 경우, 이 함수를 호출하여 변한 값을 저장하도록 합니다.
+    public void save_setter(){
+        si.setInt(getApplicationContext(), "WateringCan",wateringCan);
+        si.setInt(getApplicationContext(), "Watered",wateredCount);
+    }
+
+
     // 꽃 배치해주는 함수
+    // 이부분 나중에 수정해야 합니다.
     public void setFlower(){
+        // defaultPlace Index때 쓸 변수
+        int defaultPlaceStartCount, defaultPlaceEndCount;
+
+
         wateredCount = si.getInt(getApplicationContext(),"Watered");
 
         // 꽃 배치 인덱스 설정
@@ -167,4 +181,11 @@ public class vector_test extends AppCompatActivity {
     }
 
 
+
+    /*** 편의성을 위한 옵션 ***/
+    // 로그 출력을 위한 함수
+    public void logs(String s){
+        Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
+        System.out.println(s);
+    }
 }
